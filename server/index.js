31a6a3eefa9793dbe3e6715dc3072e7fb28082e4
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2024-11-13 18:51:48 +0545

    added typedefs and resolvers

--- server/index.js
+++ server/index.js
@@ -3,15 +3,21 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 
 const app = express();
+app.use(express.json());
 const server = new ApolloServer({
   typeDefs: `
         type Todo{
             id: ID!
             title:String!
-            completed:boolean
+            completed:Boolean
         }
 
+    type Query{
+    getTodos:[Todo]    
+    }
+
     `,
+  resolvers: {},
 });
 await server.start();
 
