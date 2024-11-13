143ada880457938de7d5838f839d850942c09d24
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2024-11-13 19:09:00 +0545

    another type and resolver added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import axios from "axios";
 
 const app = express();
 app.use(express.json());
@@ -12,12 +13,31 @@ const server = new ApolloServer({
             completed:Boolean
         }
 
+        type User{
+            id: ID!
+            name:String!
+            username:String
+            email:String!
+            phone:String!
+        }
+
+
     type Query{
     getTodos:[Todo]    
+    getAllUsers:[User]
+
     }
 
     `,
-  resolvers: {},
+  resolvers: {
+    Query: {
+      getTodos: async () =>
+        (await axios.get("https://jsonplaceholder.typicode.com/todos/")).data,
+
+      getAllUsers: async () =>
+        (await axios.get("https://jsonplaceholder.typicode.com/users/")).data,
+    },
+  },
 });
 await server.start();
 
